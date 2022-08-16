410f2d0d2b6f8f742692a22b70c333b41d0edb7c
Diego Garcia <diegofabriciogarcia@gmail.com>
2022-08-15 20:29:21 -0600

    ip

--- lib/constans.dart
+++ lib/constans.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-// const API_URL = 'http://localhost:8080/api/';
-// const url = 'http://localhost:8080/';
-const API_URL = 'http://52.201.255.173:8080/api/';
-const url = 'http://52.201.255.173:8080/';
+const API_URL = 'http://localhost:8080/api/';
+const url = 'http://localhost:8080/';
+// const API_URL = 'http://52.201.255.173:8080/api/';
+// const url = 'http://52.201.255.173:8080/';
 GlobalKey<ScaffoldMessengerState> snackbarKey =
     GlobalKey<ScaffoldMessengerState>();
